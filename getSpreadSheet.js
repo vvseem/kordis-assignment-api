c18654455eec65c6a3cfa6c7fbac00ba2b8790f6
vvseem <waseemahmedrafiq@gmail.com>
2023-06-23 20:16:28 +0500

    testing fs

--- getSpreadSheet.js
+++ getSpreadSheet.js
@@ -1,10 +1,11 @@
-// const { readFile, utils, set_fs } = require("xlsx");
-// const fs = require("fs");
-// set_fs(fs);
-//
-// const N_SPACES = 3;
-// const SHEET_PATH = "sheets/example_spreadsheet.xlsx";
-// const RANGE = utils.decode_range("A5:G91");
+const { readFile, utils, set_fs } = require("xlsx");
+const fs = require("fs");
+set_fs(fs);
+
+const N_SPACES = 3;
+const SHEET_PATH = "sheets/example_spreadsheet.xlsx";
+const RANGE = utils.decode_range("A5:G91");
+
 //
 // const getLabelAndLevel = (value, index) => ({
 //   label: value.trim(),
