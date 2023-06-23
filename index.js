e13baa1231d4d6143fe3826ee753145774061837
vvseem <waseemahmedrafiq@gmail.com>
2023-06-23 19:55:07 +0500

    Reverted code

--- index.js
+++ index.js
@@ -1,19 +1,39 @@
-import cors from "cors";
-import express from "express";
-import { router } from "./route.js";
+// import cors from "cors";
+// import express from "express";
+// import { router } from "./route.js";
+//
+// const PORT = 4000;
+// export const app = express();
+//
+// app.use(cors());
+// app.use(express.json());
+//
+// // app.use("/api", router);
+//
+// app.get("/", (req, res) => {
+//   res.send("Hey this is my API running 🥳");
+// });
+//
+// app.listen(PORT, () => {
+//   console.log(`API listening on PORT ${PORT}`);
+// });
 
+const express = require("express");
+
+const app = express();
 const PORT = 4000;
-export const app = express();
 
-app.use(cors());
-app.use(express.json());
-
-app.use("/api", router);
+app.listen(PORT, () => {
+  console.log(`API listening on PORT ${PORT} `);
+});
 
 app.get("/", (req, res) => {
   res.send("Hey this is my API running 🥳");
 });
 
-app.listen(PORT, () => {
-  console.log(`API listening on PORT ${PORT}`);
+app.get("/about", (req, res) => {
+  res.send("This is my about route..... ");
 });
+
+// Export the Express API
+module.exports = app;
