ebe12bc8eafec87a0db988b5f2a64228d9e0427b
vvseem <waseemahmedrafiq@gmail.com>
2023-06-23 20:03:56 +0500

    updated response

--- index.js
+++ index.js
@@ -1,23 +1,3 @@
-// import cors from "cors";
-// import express from "express";
-// import { router } from "./route.js";
-//
-// const PORT = 4000;
-// export const app = express();
-//
-// app.use(cors());
-// app.use(express.json());
-//
-// // app.use("/api", router);
-//
-// app.get("/", (req, res) => {
-//   res.send("Hey this is my API running 🥳");
-// });
-//
-// app.listen(PORT, () => {
-//   console.log(`API listening on PORT ${PORT}`);
-// });
-
 const express = require("express");
 const getSpreadSheet = require("./getSpreadSheet");
 
@@ -30,7 +10,7 @@ app.listen(PORT, () => {
 
 app.get("/", (req, res) => {
   const spreadsheet = getSpreadSheet();
-  res.status(200).json(spreadsheet);
+  res.send(JSON.stringify(spreadsheet));
 });
 
 app.get("/about", (req, res) => {
